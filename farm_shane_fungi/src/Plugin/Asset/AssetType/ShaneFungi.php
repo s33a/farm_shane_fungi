1aa791309d3f966d8035418b275d6fddaf03e538
s33a <81595892+s33a@users.noreply.github.com>
2021-04-16 14:03:46 -0700

    Add files via upload

---
+++ farm_shane_fungi/src/Plugin/Asset/AssetType/ShaneFungi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\farm_shane_fungi\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the fungi asset type.
+ *
+ * @AssetType(
+ *   id = "fungi",
+ *   label = @Translation("Fungi"),
+ * )
+ */
+class ShaneFungi extends FarmAssetType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'fungi_type' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Fungi species/variety'),
+        'description' => "Enter this fungi asset's species/variety.",
+        'target_type' => 'taxonomy_term',
+        'target_bundle' => 'shane_fungi_type',
+        'auto_create' => TRUE,
+        'required' => TRUE,
+        'weight' => [
+          'form' => -90,
+          'view' => -90,
+        ],
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
+}
