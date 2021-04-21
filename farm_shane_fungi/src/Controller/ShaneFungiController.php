ed39652cada4ce0787f17adcc7e842470dc68e33
s33a <shane_aldrich@onmail.com>
2021-04-21 14:26:36 -0700

    testing routing and cotroller to display overview page

---
+++ farm_shane_fungi/src/Controller/ShaneFungiController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\farm_shane_fungi\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class ExampleController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function overviewPage() {
+    return [
+      '#markup' => 'Hello, world',
+    ];
+  }
+
+}
