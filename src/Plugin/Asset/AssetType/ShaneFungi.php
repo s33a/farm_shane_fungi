a0fda08c4eca0c8316cac543c9490f9df844061c
s33a <shane_aldrich@onmail.com>
2021-04-27 16:23:25 -0700

    removed some dependencies

---
+++ src/Plugin/Asset/AssetType/ShaneFungi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_shane_fungi\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the Shane fungi asset type.
+ *
+ * @AssetType(
+ *   id = "shane_fungi",
+ *   label = @Translation("Shane Fungi"),
+ * )
+ */
+class ShaneFungi extends FarmAssetType {
+
+}
