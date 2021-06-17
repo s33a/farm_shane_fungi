3453999a8586436c134eea2020c152aa84fe1a95
s33a <shane_aldrich@onmail.com>
2021-06-17 15:36:22 -0700

    removed shane from file names and associated code

--- src/Controller/FungiController.php
+++ src/Controller/FungiController.php
@@ -1,12 +1,12 @@
-<?php
-namespace Drupal\farm_shane_fungi\Controller;
+< ?php
+namespace Drupal\farm_fungi\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 /**
  * Provides route responses for the Example module.
  */
-class ShaneFungiController extends ControllerBase {
+class FungiController extends ControllerBase {
 
   /**
    * Returns a simple page.
