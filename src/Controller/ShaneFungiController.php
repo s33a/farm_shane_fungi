07fd4cd3397598041c4848101b4f7aca657d52b3
s33a <shane_aldrich@onmail.com>
2021-07-27 13:19:13 -0700

    Removed `Fungi.php`. Renamed module to `farm_shane_fungi`. Changed filenames and code to add `shane`

--- src/Controller/ShaneFungiController.php
+++ src/Controller/ShaneFungiController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Drupal\farm_fungi\Controller;
+namespace Drupal\farm_shane_fungi\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 /**
  * Provides route responses for the Example module.
  */
-class FungiController extends ControllerBase {
+class ShaneFungiController extends ControllerBase {
 
   /**
    * Returns a simple page.
